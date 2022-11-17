15aa9ccc2c231bdeb2cd3666d3ab2302d7dc281a
Laboratorio-Info <kellyfonsecadefranca@gmail.com>
2022-11-17 20:07:06 -0300

    novo

--- app/src/main/java/br/com/etecia/foodfast/PorcoesFragment.java
+++ app/src/main/java/br/com/etecia/foodfast/PorcoesFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class PorcoesFragment extends Fragment {
@@ -22,9 +23,21 @@ public class PorcoesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_porcoes, container, false);
 
         edtPorcoes = view.findViewById(R.id.edtPorcoes);
-        btnEscolha - view.findViewById(R.id.btnEscolha);
+        btnEscolha = view.findViewById(R.id.btnEscolha);
+
+        btnEscolha.setOnClickListener(new View.OnClickListener(){
+
+          @Override
+                  public void  onClick(View view){
+                String nome = edtPorcoes.getText().toString();
+              Toast.makeText(getContext(),
+                       "Você escolheu " + nome,
+                        Toast.LENGTH_SHORT).show();
 
 
+          }
+
+      });
 
 
           return view;
