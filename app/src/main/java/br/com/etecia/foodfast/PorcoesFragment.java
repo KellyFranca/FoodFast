e7814cb78059ad6f79ce7115feb8128c82852823
Laboratorio-Info <kellyfonsecadefranca@gmail.com>
2022-11-17 19:57:10 -0300

    novo

---
+++ app/src/main/java/br/com/etecia/foodfast/PorcoesFragment.java
@@ -0,0 +1,32 @@
+package br.com.etecia.foodfast;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class PorcoesFragment extends Fragment {
+    EditText edtPorcoes;
+    Button btnEscolha;
+
+      @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_porcoes, container, false);
+
+        edtPorcoes = view.findViewById(R.id.edtPorcoes);
+        btnEscolha - view.findViewById(R.id.btnEscolha);
+
+
+
+
+          return view;
+    }
+}
